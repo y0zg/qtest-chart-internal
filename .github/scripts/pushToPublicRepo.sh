82770983fe4823afd161a3c42c5e6a7a09e6b65f
mtricentis <83463324+mtricentis@users.noreply.github.com>
2021-09-23 21:56:37 -0400

    Create pushToPublicRepo.sh
    Script added for Manual workflow push latest changes to public repo for each release

---
+++ .github/scripts/pushToPublicRepo.sh
@@ -0,0 +1,40 @@
+#!/bin/sh -ex
+
+
+FOLDER="$1"
+GITHUB_USERNAME="$2"
+GITHUB_REPO="$3"
+
+HELM_CHART_VERSION="$4"
+QTEST_MGR_VERSION="$5"
+
+CLONE_DIR=output_clone
+echo 'check current directory'
+#make directory
+mkdir /home/runner/work/$GITHUB_USERNAME
+
+cd /home/runner/work/$GITHUB_USERNAME
+pwd
+apt-get update && apt-get install git
+#apt-get add --no-cache git
+
+git config --global user.email "mtricentis@g.com"
+git config --global user.name "$GITHUB_USERNAME"
+git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/qtest-chart.git" $CLONE_DIR
+
+echo 'check current directory after Clone'
+cd $CLONE_DIR
+echo "After cd $CLONE_DIR"
+git checkout -b $GITHUB_REPO
+cp -r $FOLDER/* .
+
+sed -i 's/\(.*version:.*\)/version: '"$HELM_CHART_VERSION"'/g' chart.yml
+sed -i 's/\(.*appVersion:.*\)/appVersion: '"$QTEST_MGR_VERSION"'/g' chart.yml
+
+git add --all
+git commit --message "Update from $GITHUB_REPOSITORY"
+
+git push origin $GITHUB_REPO --force
+
+cd ..
+echo "Done!"
