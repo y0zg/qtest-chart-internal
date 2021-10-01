bc292ff9f6e262792e2634c58e8d33f4a11a4d82
brandon <b.yang@tricentis.com>
2021-10-01 11:52:32 -0400

    Fix updateversion.sh

--- Charts/qtest-chart/updateversion.sh
+++ Charts/qtest-chart/updateversion.sh
@@ -1,16 +1,18 @@
-str1="$(grep -w 'version:' Chart.yaml)"
+str1="$( grep -w 'version:' Chart.yaml | grep -v 'version: \"*\"')"
 minorVS="${str1##*.}"
 echo Current version $str1
 minorVS=$((minorVS+1))
 str2="${str1%.*}.$minorVS"
 echo Update to version $str2
-sed -i "s/version:.*/$str2/g" Chart.yaml
+sed -i '' "/\"/!s/version:.*/$str2/g" Chart.yaml
 
+# POM_VERSION="10.5.1"
+# COMMIT="06cc314"
 str3="$(grep -w 'appVersion:' Chart.yaml)"
 echo Current appVersion $str3
-minorVS="${str3##*.}"
-minorVS="${minorVS%?}"
-minorVS=$((minorVS+1))
-str4="${str3%.*}.$minorVS\""
+# minorVS="${str3##*.}"
+# minorVS="${minorVS%?}"
+# minorVS=$((minorVS+1))
+str4="${POM_VERSION}-${COMMIT}"
 echo Update to appVersion $str4
-sed -i "s/appVersion:.*/$str4/g" Chart.yaml
+sed -i '' "s/appVersion:.*/appVersion: \"$str4\"/g" Chart.yaml
